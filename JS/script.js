67d7106a2f368ef4ff5cdc207b584821d60ca539
Nuno Pinto <125605295+NunoRicardoPinto@users.noreply.github.com>
2026-08-24 02:17:27 +0100

    Adicionei a função que e permite receber email de pessoas interessadas e talvez recrutadores. Isso deois de algumas pesquisas e tudo sem precisar usar base de dados. Somente um serviço gratuito da internet. Estou feliz. daqui a pouco estará pronto se jeová me permitir.

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+// Para inicializar a função com a minha public Key
+emailjs.init("MmSSE6-kzT_N7zuIK");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); //Para não recarregar a tela depois de clicar no botão submit.(É um psdrão dele.)
+
+    // Criação de objecto dos dados ao dubmeter eles.
+    const formData = {
+      // O value serve para pegar o valor que está no campo
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      assunto: document.getElementById("assunto").value,
+      mensagem: document.getElementById("mensagem").value,
+    };
+
+    console.table(formData);
+
+    const serveceID = "service_imf1zbo";
+    const templeteID = "template_qxmcpgw";
+    // Para enviar o emai para mim
+    emailjs.send(serveceID, templeteID, formData);
+  });
